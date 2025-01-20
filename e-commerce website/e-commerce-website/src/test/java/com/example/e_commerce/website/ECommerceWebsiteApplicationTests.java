e1d92564fabc598ec01739c6b3c7a19ce3a40f44
shraddha-sadavarte <shraddhasadavarte24@gmail.com>
2025-01-20 18:08:26 +0530

    initial commit

---
+++ e-commerce website/e-commerce-website/src/test/java/com/example/e_commerce/website/ECommerceWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e_commerce.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
