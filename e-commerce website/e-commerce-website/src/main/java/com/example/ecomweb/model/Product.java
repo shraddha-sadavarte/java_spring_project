a2c3d1339f6a33898623f8f36417f5c0e280f959
shraddha-sadavarte <shraddhasadavarte24@gmail.com>
2025-01-20 18:38:01 +0530

    repository and model is created with database configurations

---
+++ e-commerce website/e-commerce-website/src/main/java/com/example/ecomweb/model/Product.java
@@ -0,0 +1,50 @@
+package com.example.ecomweb.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String description;
+    private double price;
+
+    //Getters and Setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
